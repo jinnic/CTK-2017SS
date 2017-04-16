5a9fc689abc5e2c07e88d3a6490e3f69c07d5432
j-js <hyoo04@risd.edu>
2017-04-16 03:12:59 -0400

    mobile version horizontal split added

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ var fadeOnScroll = function () {
     var images = [".image01",".image02",".image03",".image04",".image05"];
     
     
-    var scrollPos = $(window).scrollTop(),
+    var docWidth = $(document).width(),
+        scrollPos = $(window).scrollTop(),
         docHeight = $(document).height(),
         scrollPercentage = scrollPos / docHeight,
         uniqueValue = scrollPercentage * images.length;
-    for (var i = 1; i <= images.length; i++) {
-        var opacityValue = i - uniqueValue;
-        $(images[i-1]).css("opacity", opacityValue);
-        console.log(scrollPercentage);
+    
+    
+    if(docWidth<=768){
+        
+        uniqueValue = scrollPercentage * 1.2 * images.length;
+        for (var i = 1; i <= images.length; i++) {
+            var opacityValue = i - uniqueValue;
+            $(images[i-1]).css("opacity", opacityValue);
+            console.log(scrollPercentage);
+        }
+    }else{
+        uniqueValue = scrollPercentage * images.length;
+        for (var i = 1; i <= images.length; i++) {
+            var opacityValue = i - uniqueValue;
+            $(images[i-1]).css("opacity", opacityValue);
+            console.log(scrollPercentage);
+        }
     }
     
 //    $(window).scrollTop() + $(window).height() === $(document).height() && $(window).width() > 992 && $(document).scrollTop(0);
@@ -20,4 +34,8 @@ var fadeOnScroll = function () {
 
  $(window).on("scroll", function () {
     fadeOnScroll();
+});
+
+screen.addEventListener("orientationchange", function () {
+  console.log("The orientation of the screen is: " + screen.orientation);
 });
