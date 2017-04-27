a06385ffb36a675f67d33f976a8e15a06bd87316
j-js <hyoo04@risd.edu>
2017-04-27 15:42:22 -0400

    trying onscroll on div

--- main.js
+++ main.js
@@ -16,30 +16,34 @@ var fadeOnScroll = function () {
         scrollDiv = $("#scrollContainer"),
         scrollPos = scrollDiv.scrollTop(),
         docHeight = scrollDiv.height(),
-        scrollPercentage = scrollPos / docHeight,
-        uniqueValue = scrollPercentage * (images.length),
-        opacityValue,
-        containerBG = $("#container").css("background-color");
+        scrollPercentage = scrollPos / docHeight;
+//        uniqueValue = scrollPercentage * (images.length),
+//        opacityValue,
+//        containerBG = $("#container").css("background-color");
 
     
-    if(docWidth<=768 && containerBG === "rgb(0, 0, 0)"){
-        
-        uniqueValue = scrollPercentage * 1.2 * images.length;
-        for (var i = 1; i <= images.length; i++) {
-             opacityValue = i - uniqueValue;
-            $(images[i-1]).css("opacity", opacityValue);
-            console.log(containerBG);
-        }
-    }else{
-        uniqueValue = scrollPercentage * (images.length + 1);
-        for (var i = 1; i <= images.length; i++) {
-             opacityValue = i - uniqueValue;
-            $(images[i-1]).css("opacity", opacityValue);
-            console.log('uniqueVal: ' + uniqueValue);
+//    if(docWidth<=768 && containerBG === "rgb(0, 0, 0)"){
+//        
+//        uniqueValue = scrollPercentage * 1.2 * images.length;
+//        for (var i = 1; i <= images.length; i++) {
+//             opacityValue = i - uniqueValue;
+//            $(images[i-1]).css("opacity", opacityValue);
+//            console.log(containerBG);
+//        }
+//    }else{
+//        uniqueValue = scrollPercentage * (images.length + 1);
+//        for (var i = 1; i <= images.length; i++) {
+//             opacityValue = i - uniqueValue;
+//            $(images[i-1]).css("opacity", opacityValue);
+//            console.log('uniqueVal: ' + uniqueValue);
+//            console.log('scrollPos: ' + scrollPos);
+//            console.log('docHeight: ' + docHeight);
+//        }
+//    }
+    
+    
             console.log('scrollPos: ' + scrollPos);
             console.log('docHeight: ' + docHeight);
-        }
-    }
     
 //    $(window).scrollTop() + $(window).height() === $(document).height() && $(window).width() > 992 && $(document).scrollTop(0);
     
@@ -55,21 +59,23 @@ var fadeScroll = function(){
 };
 */
 
- $(window).on("scroll", function () {
-    
-    fadeOnScroll();
-      
-     $('video').each(function(){
-        if ($(this).is(':in-viewport')) {
-            $(this)[0].play();
-            console.log("video in viewport");
-        } else {
-            $(this)[0].pause();
-            console.log("video NOT in viewport");
-        }
-    });
-//     console.log($("#container").css("background-color"));
-});
+fadeOnScroll();
+
+// $("#scrollContainer").on("scroll", function () {
+//    
+//    fadeOnScroll();
+//      
+//     $('video').each(function(){
+//        if ($(this).is(':in-viewport')) {
+//            $(this)[0].play();
+//            console.log("video in viewport");
+//        } else {
+//            $(this)[0].pause();
+//            console.log("video NOT in viewport");
+//        }
+//    });
+////     console.log($("#container").css("background-color"));
+//});
 
 
  
