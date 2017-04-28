25a70e56b42697b7dc9f085867a08fe3848cb923
j-js <hyoo04@risd.edu>
2017-04-28 13:19:53 -0400

    testing with test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+document.getElementById("scrollContainer").onscroll = function() {detactY()};
+
+
+function detactY(){
+    
+    console.log("scrollContainer is on scroll" );
+
+//    var elmnt = document.getElementsByClassName("scroll-image:first");
+//    var x = elmnt.scrollLeft;
+//    var y = elmnt.scrollTop;
+//    //var windowY = document.window.scroll;
+//    console.log("current y position of first croll-image" + y);
+
+};
