d9cc767fa082fe52a7615102ca0eec8d749c66fe
j-js <hyoo04@risd.edu>
2017-04-29 01:11:50 -0400

    in mobile : moving last image transition along scroll-image

--- test.js
+++ test.js
@@ -25,9 +25,11 @@ $(document).ready(function() {
 			       if ($(this).is(':in-viewport')) {
 			           $(this)[0].play();
 			           console.log("video in viewport");
+
 			       } else {
 			           $(this)[0].pause();
 			           console.log("video NOT in viewport");
+			           $(".trans-image:first").css("top",0 + "vh")
 			       }
 			  	});
 
@@ -52,11 +54,23 @@ $(document).ready(function() {
 	    
 	   if(docWidth<=768 && containerBG === "rgb(0, 0, 0)"){
 	       $("#scrollContainer").css("min-height",50*images.length+"vh");
-	       for (var i = 1; i <= images.length; i++) {
+	       for (var i = 1; i <= images.length -1; i++) {
 	            opacityValue = i - uniqueValue;
 	           $(images[i-1]).css("opacity", opacityValue);
 	           // console.log(containerBG);
 	           console.log("fast scroll");
+
+	           console.log('uniqueVal: ' + uniqueValue);
+	           console.log('scrollPos: ' + scrollPos);
+	           console.log('scrollPercentage: ' + scrollPercentage);
+
+	           if($(images[3]).css("opacity") <= 0){
+
+	           	var movePos = -(uniqueValue-4)*100/2;
+
+	           		$(".trans-image:first").css("top",movePos + "vh")
+	           	console.log(movePos);
+	           }
 	       }
 	   }else{
 
